b3c4b9722871c890d2404b04115fafabee587044
Mehdi <mehdiennajehfr@gmail.com>
2026-02-04 12:59:44 +0000

    feat: Implement graceful shutdown by listening for termination signals.

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-    fmt.Println("Moteur de recommandation Sport démarré")
+	fmt.Println("Moteur de recommandation Sport démarré")
+
+	// Wait for termination signal to keep the process alive
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+	fmt.Println("Arrêt du moteur...")
 }
